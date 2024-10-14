aa1b255e034b02a8155ab52cb840967b6076725a
Kadafa69-96 <ckadafa@gmail.com>
2024-10-14 13:18:52 +0300

    Add files via upload

---
+++ String-data.py
@@ -0,0 +1,17 @@
+myString = "This is a string nigga."
+print(myString)
+
+print(type(myString))
+print(myString + " is of the data type " + str(type(myString)))
+
+firstString= "water"
+secondString= "fall"
+thirdrdString= firstString + secondString
+print(thirdrdString)
+
+name = input("what is your name? ")
+print(name)
+
+color = input("What is your fav color? ")
+animal = input("What is yout fav animal? ")
+print("{}, you like a {} {}! ".format(name,color,animal))
---
+++ hello-world.py
@@ -0,0 +1,1 @@
+print("Hello Weh Mzenga")
